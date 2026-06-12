3e5112fb4682a18c20ebacf429e11bf159fecb7b
A_GARRIGON-VIRAMOUTOU <adrien.garrigon-viramoutou@etu.u-pec.fr>
2026-06-12 15:10:17 +0200

    ajout de commentaires

--- controllers/api.py
+++ controllers/api.py
@@ -4,7 +4,8 @@ from models.dimensions import Departement
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
 
-@bp_api.route("/departements/<string:region_code>")
+@bp_api.route("/departements/<string:region_code>") 
+# Correction de region_id par region_code (c'est un string et non un int)
 def departements(region_code):
     """Retourne les départements d'une région au format JSON."""
     session = Session()
