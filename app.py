45efd0a6b5209ffdb147ac07e9c5ad49d082fa89
A_GARRIGON-VIRAMOUTOU <adrien.garrigon-viramoutou@etu.u-pec.fr>
2026-06-12 09:55:11 +0200

    Implémentation Route JSON côté serveur

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config
 from controllers.accueil import bp_accueil
+from controllers.api import bp_api
 
 app = Flask(__name__)
+app.register_blueprint(bp_api)
 app.config.from_object(Config)
 # Enregistrement des contrôleurs (blueprints)
 app.register_blueprint(bp_accueil)
