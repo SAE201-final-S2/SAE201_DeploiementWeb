3812ee288670e7a64b5379bfe0e19c895911146e
A_GARRIGON-VIRAMOUTOU <adrien.garrigon-viramoutou@etu.u-pec.fr>
2026-06-15 11:13:41 +0200

    complétion du TUTORIEL 2

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from controllers.accueil import bp_accueil
 from controllers.api import bp_api
-# from controllers.effectifs import bp_effectifs 
+from controllers.effectifs import bp_effectifs 
 
 app = Flask(__name__)
 app.register_blueprint(bp_api)
-# app.register_blueprint(bp_effectifs)
+app.register_blueprint(bp_effectifs)
 app.config.from_object(Config)
 # Enregistrement des contrôleurs (blueprints)
 app.register_blueprint(bp_accueil)
 if __name__ == "__main__":
  app.run(debug=True)
+@app.errorhandler(404)
+def page_non_trouvee(e):
+ return render_template("erreur.html",
+ message="Page non trouvée."), 404
+@app.errorhandler(500)
+def erreur_serveur(e):
+ return render_template("erreur.html",
+ message="Erreur interne. Réessayez plus tard."), 500
+
