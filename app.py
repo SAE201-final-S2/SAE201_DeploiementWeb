2e549274a1bba225c85aa157237c59bc9aa42829
A_GARRIGON-VIRAMOUTOU <adrien.garrigon-viramoutou@etu.u-pec.fr>
2026-06-23 11:41:22 +0200

    Ajout de la page Comparaison

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from controllers.accueil import bp_accueil
 from controllers.api import bp_api
 from controllers.effectifs import bp_effectifs 
 from controllers.carte import bp_carte
+from controllers.comparaison import bp_comparaison
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.register_blueprint(bp_api)
 app.register_blueprint(bp_effectifs)
-app.config.from_object(Config)
 # Enregistrement des contrôleurs (blueprints)
 app.register_blueprint(bp_accueil)
+app.register_blueprint(bp_comparaison)
 app.register_blueprint(bp_carte)
 
 @app.errorhandler(404)
